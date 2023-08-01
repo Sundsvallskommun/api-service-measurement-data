51089267e096a4a416ce4d991345d2cc1d426029
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2023-08-01 12:46:26 +0200

    UF-5572: Fix code smells.

--- src/test/java/se/sundsvall/measurementdata/api/model/example/ResponseExamplesTest.java
+++ src/test/java/se/sundsvall/measurementdata/api/model/example/ResponseExamplesTest.java
@@ -1,45 +1,48 @@
 package se.sundsvall.measurementdata.api.model.example;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import se.sundsvall.measurementdata.api.model.Data;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import static se.sundsvall.measurementdata.api.model.example.ResponseExamples.COMMUNICATION_RESPONSE_EXAMPLE;
 import static se.sundsvall.measurementdata.api.model.example.ResponseExamples.DISTRICT_HEATING_RESPONSE_EXAMPLE;
 import static se.sundsvall.measurementdata.api.model.example.ResponseExamples.ELECTRICITY_RESPONSE_EXAMPLE;
 import static se.sundsvall.measurementdata.api.model.example.ResponseExamples.WASTE_MANAGEMENT_RESPONSE_EXAMPLE;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+
+import se.sundsvall.measurementdata.api.model.Data;
+
 class ResponseExamplesTest {
 
-    private ObjectMapper mapper;
+	private ObjectMapper mapper;
 
-    @BeforeEach
-    void config() {
-        this.mapper = JsonMapper.builder()
-                .findAndAddModules()
-                .build();
-    }
+	@BeforeEach
+	void config() {
+		this.mapper = JsonMapper.builder()
+			.findAndAddModules()
+			.build();
+	}
 
-    @Test
-    void verifyDistrictHeatingExample() throws JsonProcessingException {
-        mapper.readValue(DISTRICT_HEATING_RESPONSE_EXAMPLE, Data.class);
-    }
+	@Test
+	void verifyDistrictHeatingExample() throws JsonProcessingException {
+		assertThat(mapper.readValue(DISTRICT_HEATING_RESPONSE_EXAMPLE, Data.class)).isInstanceOf(Data.class);
+	}
 
-    @Test
-    void verifyCommunicationExample() throws JsonProcessingException {
-        mapper.readValue(COMMUNICATION_RESPONSE_EXAMPLE, Data.class);
-    }
+	@Test
+	void verifyCommunicationExample() throws JsonProcessingException {
+		assertThat(mapper.readValue(COMMUNICATION_RESPONSE_EXAMPLE, Data.class)).isInstanceOf(Data.class);
+	}
 
-    @Test
-    void verifyElectricityExample() throws JsonProcessingException {
-        mapper.readValue(ELECTRICITY_RESPONSE_EXAMPLE, Data.class);
-    }
+	@Test
+	void verifyElectricityExample() throws JsonProcessingException {
+		assertThat(mapper.readValue(ELECTRICITY_RESPONSE_EXAMPLE, Data.class)).isInstanceOf(Data.class);
+	}
 
-    @Test
-    void verifyWasteExample() throws JsonProcessingException {
-        mapper.readValue(WASTE_MANAGEMENT_RESPONSE_EXAMPLE, Data.class);
-    }
+	@Test
+	void verifyWasteExample() throws JsonProcessingException {
+		assertThat(mapper.readValue(WASTE_MANAGEMENT_RESPONSE_EXAMPLE, Data.class)).isInstanceOf(Data.class);
+	}
 }
