bd0099b2f037427cd42243e00763a0ede14f117e
cheezi0747 <linus.sjolinder@b3.se>
2024-12-10 10:56:16 +0100

    UF-11274: Fix deperacted mockbean

--- src/test/java/se/sundsvall/measurementdata/api/MeasurementDataResourceFailuresTest.java
+++ src/test/java/se/sundsvall/measurementdata/api/MeasurementDataResourceFailuresTest.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.util.LinkedMultiValueMap;
@@ -32,7 +32,7 @@ class MeasurementDataResourceFailuresTest {
 	@Autowired
 	private WebTestClient webTestClient;
 
-	@MockBean
+	@MockitoBean
 	private MeasurementDataService serviceMock;
 
 	@Test
