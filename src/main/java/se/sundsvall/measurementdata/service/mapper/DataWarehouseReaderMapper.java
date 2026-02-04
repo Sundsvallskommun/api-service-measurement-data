fd8b288fdab0e65d05837ed367c7dff3c18d8123
AndreSoderlund <andre.soderlund@b3.se>
2026-02-04 13:45:06 +0100

    Updated DWR integration. Added some extra unit tests and updated integration tests.

--- src/main/java/se/sundsvall/measurementdata/service/mapper/DataWarehouseReaderMapper.java
+++ src/main/java/se/sundsvall/measurementdata/service/mapper/DataWarehouseReaderMapper.java
@@ -1,20 +1,13 @@
 package se.sundsvall.measurementdata.service.mapper;
 
 import static java.util.Collections.emptyList;
-import static java.util.Objects.isNull;
-import static org.apache.commons.lang3.math.NumberUtils.INTEGER_ZERO;
 
-import generated.se.sundsvall.datawarehousereader.Aggregation;
-import generated.se.sundsvall.datawarehousereader.Category;
 import generated.se.sundsvall.datawarehousereader.Measurement;
-import generated.se.sundsvall.datawarehousereader.MeasurementMetaData;
-import generated.se.sundsvall.datawarehousereader.MeasurementResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import se.sundsvall.measurementdata.api.model.Data;
 import se.sundsvall.measurementdata.api.model.MeasurementDataSearchParameters;
 import se.sundsvall.measurementdata.api.model.MeasurementPoint;
@@ -26,59 +19,33 @@ public class DataWarehouseReaderMapper {
 
 	private DataWarehouseReaderMapper() {}
 
-	public static Aggregation toAggregation(se.sundsvall.measurementdata.api.model.Aggregation aggregation) {
-		if (isNull(aggregation)) {
-			return null;
-		}
-
-		return switch (aggregation) {
-			case QUARTER -> Aggregation.QUARTER;
-			case HOUR -> Aggregation.HOUR;
-			case DAY -> Aggregation.DAY;
-			case MONTH -> Aggregation.MONTH;
-		};
-	}
-
-	public static Category toCategory(se.sundsvall.measurementdata.api.model.Category category) {
-		if (isNull(category)) {
-			return null;
-		}
-
-		return switch (category) {
-			case DISTRICT_HEATING -> Category.DISTRICT_HEATING;
-			case ELECTRICITY -> Category.ELECTRICITY;
-			case COMMUNICATION -> Category.COMMUNICATION;
-			case WASTE_MANAGEMENT -> Category.WASTE_MANAGEMENT;
-		};
-	}
-
-	public static Data toData(MeasurementDataSearchParameters parameters, MeasurementResponse response) {
+	public static Data toData(final MeasurementDataSearchParameters parameters, final List<Measurement> measurements) {
 		return Data.create()
 			.withAggregateOn(parameters.getAggregateOn())
 			.withCategory(parameters.getCategory())
 			.withFacilityId(parameters.getFacilityId())
 			.withFromDate(parameters.getFromDate())
 			.withToDate(parameters.getToDate())
-			.withMeasurementSeries(toMeasurementSeries(response));
+			.withMeasurementSeries(toMeasurementSeries(measurements));
 	}
 
-	private static List<MeasurementSerie> toMeasurementSeries(MeasurementResponse response) {
-		if (Objects.equals(INTEGER_ZERO, response.getMeta().getCount())) {
+	private static List<MeasurementSerie> toMeasurementSeries(final List<Measurement> measurements) {
+		if (Objects.isNull(measurements) || measurements.isEmpty()) {
 			return emptyList();
 		}
 
 		final var series = new HashMap<String, MeasurementSerie>();
-		Optional.ofNullable(response.getMeasurements()).orElse(emptyList()).forEach(measurement -> handleMeasurement(series, measurement));
+		measurements.forEach(measurement -> handleMeasurement(series, measurement));
 
 		return List.copyOf(series.values());
 	}
 
-	private static void handleMeasurement(Map<String, MeasurementSerie> series, Measurement measurement) {
-		String key = measurement.getMeasurementType() + measurement.getUnit();
+	private static void handleMeasurement(final Map<String, MeasurementSerie> series, final Measurement measurement) {
+		var key = measurement.getFeedType() + measurement.getUnit();
 
 		if (!series.containsKey(key)) {
 			series.put(key, MeasurementSerie.create()
-				.withMeasurementType(measurement.getMeasurementType())
+				.withMeasurementType(measurement.getFeedType())
 				.withMeasurementPoints(new ArrayList<>())
 				.withUnit(measurement.getUnit()));
 		}
@@ -86,33 +53,20 @@ public class DataWarehouseReaderMapper {
 		addMeasurementPoint(series.get(key), measurement);
 	}
 
-	private static void addMeasurementPoint(MeasurementSerie serie, Measurement measurement) {
-		serie.getMeasurementPoints().add(MeasurementPoint.create()
-			.withMetaData(toMetaDataList(measurement))
-			.withTimestamp(measurement.getTimestamp())
-			.withValue(measurement.getValue()));
+	private static void addMeasurementPoint(final MeasurementSerie series, final Measurement measurement) {
+		series.getMeasurementPoints().add(MeasurementPoint.create()
+			.withMetaData(toMetaData(measurement))
+			.withTimestamp(measurement.getDateAndTime())
+			.withValue(measurement.getUsage()));
 	}
 
-	private static List<MetaData> toMetaDataList(Measurement measurement) {
-
-		List<MetaData> metaData = new ArrayList<>(Optional.ofNullable(measurement.getMetaData()).orElse(emptyList()).stream()
-			.map(DataWarehouseReaderMapper::toMetaData)
-			.toList());
-
-		Optional.ofNullable(measurement.getInterpolation())
-			.filter(interpolation -> interpolation > 0)
-			.ifPresent(interpolation -> metaData.add(toMetaData(KEY_INTERPOLATION, String.valueOf(interpolation))));
-
-		return metaData.isEmpty() ? null : metaData;
+	private static List<MetaData> toMetaData(final Measurement measurement) {
+		if (measurement.getInterpolation() != null && measurement.getInterpolation() > 0) {
+			return List.of(MetaData.create()
+				.withKey(KEY_INTERPOLATION)
+				.withValue(String.valueOf(measurement.getInterpolation())));
+		}
+		return null;
 	}
 
-	private static MetaData toMetaData(MeasurementMetaData metaData) {
-		return toMetaData(metaData.getKey(), metaData.getValue());
-	}
-
-	private static MetaData toMetaData(String key, String value) {
-		return MetaData.create()
-			.withKey(key)
-			.withValue(value);
-	}
 }
